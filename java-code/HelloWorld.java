c641b6ac723c243d4ac7b05698a717968626d009
Harshithamuthyala311 <harshithamuthyala3@gmail.com>
2025-08-05 09:44:08 +0530

    Add ESLint GitHub Action

---
+++ java-code/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This is a HelloWorld class.
+ */
+public class HelloWorld {
+
+  /**
+   * Main method to print a message.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
---
+++ java-code/TestBadStyle.java
@@ -0,0 +1,14 @@
+/**
+ * This is a badly styled class for testing Checkstyle.
+ */
+public class TestBadStyle {
+
+  /**
+   * Main method.
+   *
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("bad style");
+  }
+}
